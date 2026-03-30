61c012fc35a26c4a878ef764cb81fa79134c3f76
Oscar Molina <oscarjosam@gmail.com>
2026-03-29 22:17:31 -0600

    Creacion de la capa de datos
    Se crea el acceso a datos de los archivos

--- Proyecto1Progra3/src/Entidad/Persona.java
+++ Proyecto1Progra3/src/Entidad/Persona.java
@@ -4,10 +4,69 @@
  */
 package Entidad;
 
-/**
- *
- * @author oscar
- */
 public class Persona {
-    
+
+    private String cedula;
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String codigoElectoral;
+    private String sexo;
+    private int fechaNacimiento;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getCodigoElectoral() {
+        return codigoElectoral;
+    }
+
+    public void setCodigoElectoral(String codigoElectoral) {
+        this.codigoElectoral = codigoElectoral;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(int fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
