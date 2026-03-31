9d353f156b23343831355630eb2d17806d115cc1
Oscar Molina <oscarjosam@gmail.com>
2026-03-30 22:26:29 -0600

    Desarrollo del PadronService

--- Proyecto1Progra3/src/Logica_Negocio/PadronService.java
+++ Proyecto1Progra3/src/Logica_Negocio/PadronService.java
@@ -4,10 +4,27 @@
  */
 package Logica_Negocio;
 
-/**
- *
- * @author Rhos
- */
+import DTO.RespuestaPadron;
+import Datos.*;
+import Entidad.*;
+import java.util.Optional;
+
 public class PadronService {
-    
+    private RepositorioPadron repoP;
+    private RepositorioDistelec repoD;
+
+    public PadronService(RepositorioPadron p, RepositorioDistelec d) {
+        this.repoP = p; this.repoD = d;
+    }
+
+    public RespuestaPadron consultar(String cedula) {
+        if (cedula == null || cedula.isBlank()) return RespuestaPadron.error("Cédula vacía");
+        Optional<Persona> pOpt = repoP.buscarPorCedula(cedula);
+        if (pOpt.isPresent()) {
+            Direccion d = repoD.buscarPorCodigo(pOpt.get().getCodigoElectoral()).orElse(null);
+            return RespuestaPadron.exitosa(pOpt.get(), d);
+        }
+        return RespuestaPadron.error("No encontrado");
+    }
 }
+
